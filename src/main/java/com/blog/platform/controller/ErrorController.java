1012662c0b77279cae9b6194758f1b96b6afed12
makswilder <maks.shherbakov04@gmail.com>
2025-06-21 19:09:11 +0200

    feat(error): added global error handler with custom ApiErrorResponse DTO

---
+++ src/main/java/com/blog/platform/controller/ErrorController.java
@@ -0,0 +1,35 @@
+package com.blog.platform.controller;
+
+import com.blog.platform.domain.dto.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        log.error("Caught Exception", ex);
+        ApiErrorResponse err = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred!")
+                .build();
+        return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalIdentifierException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ApiErrorResponse err = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+    }
+}
