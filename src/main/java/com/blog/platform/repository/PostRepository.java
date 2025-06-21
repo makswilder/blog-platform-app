590c16eb79c363da2fb409cfd2cef4129601da3d
makswilder <maks.shherbakov04@gmail.com>
2025-06-21 18:53:51 +0200

    feat: created repositories for Post, Tag, and User

---
+++ src/main/java/com/blog/platform/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.blog.platform.repository;
+
+import com.blog.platform.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
---
+++ src/main/java/com/blog/platform/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.blog.platform.repository;
+
+import com.blog.platform.domain.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
