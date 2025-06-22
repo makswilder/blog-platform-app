a6cb8a7b7b5b3840d6cee79082127301f2be4239
makswilder <maks.shherbakov04@gmail.com>
2025-06-22 13:36:29 +0200

    feat(security): add BlogUserDetails and BlogUserDetailsService for Spring Security

---
+++ src/main/java/com/blog/platform/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.blog.platform.security;
+
+import com.blog.platform.domain.entity.User;
+import com.blog.platform.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not fount with email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
