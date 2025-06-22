c0c0524a9791cc9c2badb9bc626bb2a801b64e2a
makswilder <maks.shherbakov04@gmail.com>
2025-06-22 11:53:43 +0200

    feat(category): added endpoint to delete category with post association check

--- src/main/java/com/blog/platform/service/CategoryService.java
+++ src/main/java/com/blog/platform/service/CategoryService.java
@@ -3,9 +3,11 @@ package com.blog.platform.service;
 import com.blog.platform.domain.entity.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
 
     List<Category> listCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
 }
