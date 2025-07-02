2422fcf76e5554ae5679297e43c323c038453cb7
makswilder <maks.shherbakov04@gmail.com>
2025-07-02 13:00:27 +0200

    feat(tag): implemented TagMapper to map Tag to TagResponse with published post count

---
+++ src/main/java/com/blog/platform/domain/dto/TagResponse.java
@@ -0,0 +1,18 @@
+package com.blog.platform.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagResponse {
+    private UUID id;
+    private String name;
+    private Integer postCount;
+}
