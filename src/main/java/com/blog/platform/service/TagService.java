7942b867b980d58ce24c63572a2a8e5a5c0580bb
makswilder <maks.shherbakov04@gmail.com>
2025-07-02 16:02:08 +0200

    feat(tag): implemented createTags to save new tags and return all matching tags

--- src/main/java/com/blog/platform/service/TagService.java
+++ src/main/java/com/blog/platform/service/TagService.java
@@ -3,7 +3,9 @@ package com.blog.platform.service;
 import com.blog.platform.domain.entity.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
--- src/main/java/com/blog/platform/service/impl/TagServiceImpl.java
+++ src/main/java/com/blog/platform/service/impl/TagServiceImpl.java
@@ -3,10 +3,15 @@ package com.blog.platform.service.impl;
 import com.blog.platform.domain.entity.Tag;
 import com.blog.platform.repository.TagRepository;
 import com.blog.platform.service.TagService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +22,32 @@ public class TagServiceImpl implements TagService {
     @Override
     public List<Tag> getTags() {
         return tagRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public List<Tag> createTags(Set<String> tagNames) {
+        List<Tag> exisitingTags = tagRepository.findByNameIn(tagNames);
+
+        Set<String> existingTagNames = exisitingTags.stream()
+                .map(Tag::getName)
+                .collect(Collectors.toSet());
+
+        List<Tag> newTags = tagNames.stream()
+                .filter(name -> !existingTagNames.contains(name))
+                .map(name -> Tag.builder()
+                        .name(name)
+                        .posts(new HashSet<>())
+                        .build())
+                .toList();
+
+        List<Tag> savedTags = new ArrayList<>();
+        if(!newTags.isEmpty()) {
+            savedTags = tagRepository.saveAll(newTags);
+        }
+
+        savedTags.addAll(exisitingTags);
+
+        return savedTags;
     }
 }
