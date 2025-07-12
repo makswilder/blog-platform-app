7932fad7e26767a44b6f1d1af84955eaa55f5995
makswilder <maks.shherbakov04@gmail.com>
2025-07-12 17:48:10 +0200

    feat(tag): implemented endpoint to delete tag with post association check

--- src/main/java/com/blog/platform/service/TagService.java
+++ src/main/java/com/blog/platform/service/TagService.java
@@ -4,8 +4,10 @@ import com.blog.platform.domain.entity.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getTags();
     List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
 }
